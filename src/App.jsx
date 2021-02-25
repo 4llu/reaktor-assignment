0df8a8c4cb4044a77a6cb82a096cea7896dea25f
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2021-02-25 17:33:31 +0200

    Add Material-ui and initial theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import theme from './theme';
+
 function App() {
     const history = createBrowserHistory();
 
     return (
-        <Router history={history}>
-            <Route exact={true} path='/' component={() => <div>Hello World</div>} />
-        </Router>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router history={history}>
+                <Route exact={true} path='/' component={() => <div>Hello World</div>} />
+            </Router>
+        </ThemeProvider>
     );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,46 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// Some constants
+const base_line_height = 1.2;
+const base_font_family = ['Open Sans', 'sans-serif'].join(',');
+
+const theme = createMuiTheme({
+    palette: {
+        background: '#fff',
+    },
+    typography: {
+        fontFamily: base_font_family,
+        fontSize: 16,
+        fontWeightRegular: 400,
+        fontWeightBold: 700,
+        h1: {
+            fontSize: '2.441rem',
+            lineHeight: base_line_height * 1.5,
+            fontWeight: 700,
+        },
+        h2: {
+            fontSize: '1.15rem',
+            fontWeight: 700,
+        },
+        body1: {
+            fontSize: '1rem',
+            lineHeight: base_line_height,
+        },
+        body2: {
+            fontSize: '0.8rem',
+            lineHeight: base_line_height,
+        },
+    },
+    spacing: (factor) => `${base_line_height * factor}rem`,
+});
+
+const overrides = theme.overrides;
+overrides.MuiCssBaseline = {
+    '@global': {
+        body: {
+            fontSize: '1rem',
+        },
+    },
+};
+
+export default theme;
