448aa257aedcf02d650ab038a2984adbdb8e4ec3
Aleksanteri Hämäläinen <aleksanterih@gmail.com>
2021-02-25 19:34:28 +0200

    Create home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 
 import theme from './theme';
 
+// Views
+import Home from './views/home';
+
 function App() {
     const history = createBrowserHistory();
 
@@ -13,7 +16,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Router history={history}>
-                <Route exact={true} path='/' component={() => <div>Hello World</div>} />
+                <Route exact={true} path='/' component={Home} />
             </Router>
         </ThemeProvider>
     );
---
+++ src/views/home/index.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Container, Grid, Box, Typography, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+    links: {
+        margin: 'auto',
+    },
+    linkWrapper: {
+        position: 'relative',
+        textDecoration: 'none',
+    },
+    linkImage: {
+        maxWidth: '100%',
+        transitionDuration: theme.transitions.duration.shortest,
+        '&:hover': {
+            transform: 'scale(1.2)',
+        },
+    },
+    linkTitle: {
+        position: 'absolute',
+        bottom: theme.spacing(1),
+        left: theme.spacing(1),
+        right: theme.spacing(1),
+        backgroundColor: 'rgba(0,0,0,0.95)',
+        color: '#fff',
+        textDecoration: 'none',
+        padding: theme.spacing(1),
+    },
+}));
+
+const Home = () => {
+    const classes = useStyles();
+
+    const categories = [
+        {
+            name: 'Gloves',
+            page: 'gloves',
+            img: 'gloves.jpg',
+        },
+        {
+            name: 'Face masks',
+            page: 'face-masks',
+            img: 'face_mask.jpg',
+        },
+        {
+            name: 'Beanies',
+            page: 'beanies',
+            img: 'beanie.jpg',
+        },
+    ];
+
+    return (
+        <Container>
+            <Box my={2}>
+                <Typography variant='h1' component='h1' gutterBottom>
+                    ImagiClothes Warehouse
+                </Typography>
+            </Box>
+            <Box my={2}>
+                <Typography variant='h2' gutterBottom>
+                    Available categories
+                </Typography>
+                <Grid container spacing={2} className={classes.links}>
+                    {categories.map((category) => (
+                        <Grid
+                            item
+                            key={category.img}
+                            xs={6}
+                            sm={4}
+                            className={classes.linkWrapper}
+                            component={Link}
+                            to={`/${category.page}`}>
+                            <Box overflow='hidden'>
+                                <img
+                                    src={`/images/${category.img}`}
+                                    alt={category.name}
+                                    className={classes.linkImage}
+                                />
+                                <div className={classes.linkTitle}>{category.name}</div>
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </Container>
+    );
+};
+
+export default Home;
